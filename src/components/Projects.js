358ae223c1b1603f5ce7db6315c27a9d160986af
Matt Creese <mattycreese@gmail.com>
2022-05-27 17:13:03 +0100

    projects ordered

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -43,122 +43,121 @@ function Projects() {
             </a>
           </div>
         </div>
-        <div className='each-project'>
-          <Tilt>
-            <div id='purely' style={{ scale: 2 }}></div>
-          </Tilt>
+      </div>
+      <div className='each-project'>
+        <Tilt>
+          <div id='purely' style={{ scale: 2 }}></div>
+        </Tilt>
 
-          <div className='project-info'>
-            <h1 className='project-titles'>Purely Podcast</h1>
-            <p>
-              A 7 day group project to build a document database using mongoDB
-              which connected to a React frontend. I built the registration and
-              login functions utilising JWT token authorisation, built & tested
-              backend functions. Purely Podcast is a website which allows users
-              to find, favourite and add new podcasts. I also built some of the
-              functions that allow users to upload and edit their own podcasts.
-            </p>
-            <div className='project-links'>
-              <a
-                href='https://bit.ly/3sd1IBY'
-                target='_blank'
-                rel='noreferrer'
-                className='project-links'
-                id='purely-website'
-              >
-                <BsIcons.BsLink45Deg />
-                Go to Website
-              </a>
-              <a
-                href='https://bit.ly/3lRql3u'
-                target='_blank'
-                rel='noreferrer'
-                className='project-links'
-                id='purely-readme'
-              >
-                <AiIcons.AiOutlineGithub /> View Readme
-              </a>
-            </div>
+        <div className='project-info'>
+          <h1 className='project-titles'>Purely Podcast</h1>
+          <p>
+            A 7 day group project to build a document database using mongoDB
+            which connected to a React frontend. I built the registration and
+            login functions utilising JWT token authorisation, built & tested
+            backend functions. Purely Podcast is a website which allows users to
+            find, favourite and add new podcasts. I also built some of the
+            functions that allow users to upload and edit their own podcasts.
+          </p>
+          <div className='project-links'>
+            <a
+              href='https://bit.ly/3sd1IBY'
+              target='_blank'
+              rel='noreferrer'
+              className='project-links'
+              id='purely-website'
+            >
+              <BsIcons.BsLink45Deg />
+              Go to Website
+            </a>
+            <a
+              href='https://bit.ly/3lRql3u'
+              target='_blank'
+              rel='noreferrer'
+              className='project-links'
+              id='purely-readme'
+            >
+              <AiIcons.AiOutlineGithub /> View Readme
+            </a>
           </div>
         </div>
-        <div className='each-project'>
-          <Tilt>
-            <div id='doggopedia'></div>
-          </Tilt>
+      </div>
+      <div className='each-project'>
+        <Tilt>
+          <div id='doggopedia'></div>
+        </Tilt>
 
-          <div className='project-info'>
-            <h1 className='project-titles'>Doggopedia</h1>
-            <p>
-              This was a 2 day project to build a website which connected to an
-              API and retrieved data. I completed this project with two other
-              colleagues. I specifically built the search bar and filter
-              function with JavaScript React, tested the API with Postman and
-              completed parts of the styling with SCSS and Bulma.
-            </p>
-            <div className='project-links'>
-              <a
-                href='https://doggopedia2.netlify.app/'
-                target='_blank'
-                rel='noreferrer'
-                className='project-links'
-                id='doggopedia-website'
-              >
-                <BsIcons.BsLink45Deg />
-                Go to Website
-              </a>
-              <a
-                href='https://bit.ly/3wTapmy'
-                target='_blank'
-                rel='noreferrer'
-                className='project-links'
-                id='doggopedia-readme'
-              >
-                <AiIcons.AiOutlineGithub /> View Readme
-              </a>
-            </div>
+        <div className='project-info'>
+          <h1 className='project-titles'>Doggopedia</h1>
+          <p>
+            This was a 2 day project to build a website which connected to an
+            API and retrieved data. I completed this project with two other
+            colleagues. I specifically built the search bar and filter function
+            with JavaScript React, tested the API with Postman and completed
+            parts of the styling with SCSS and Bulma.
+          </p>
+          <div className='project-links'>
+            <a
+              href='https://doggopedia2.netlify.app/'
+              target='_blank'
+              rel='noreferrer'
+              className='project-links'
+              id='doggopedia-website'
+            >
+              <BsIcons.BsLink45Deg />
+              Go to Website
+            </a>
+            <a
+              href='https://bit.ly/3wTapmy'
+              target='_blank'
+              rel='noreferrer'
+              className='project-links'
+              id='doggopedia-readme'
+            >
+              <AiIcons.AiOutlineGithub /> View Readme
+            </a>
           </div>
         </div>
-        <div className='each-project'>
-          <Tilt>
-            <div id='nibbler'></div>
-          </Tilt>
+      </div>
+      <div className='each-project'>
+        <Tilt>
+          <div id='nibbler'></div>
+        </Tilt>
 
-          <div className='project-info'>
-            <h1 className='project-titles'>Nibbler Bytes</h1>
-            <p>
-              a 7 day solo project to test the skills I had learned up until
-              that point. The remit of the task was to build a grid based game
-              using Vanilla JavaScript, HTML and CSS. Links to the project and
-              readme can be found below:
-            </p>
-            <div className='project-links'>
-              <a
-                href='https://bit.ly/37ZMiKK'
-                target='_blank'
-                className='project-links'
-                rel='noreferrer'
-                id='nibbler-website'
-              >
-                <BsIcons.BsLink45Deg />
-                Go to Website
-              </a>
-              <br />
-              <a
-                href='https://bit.ly/3PHo1df'
-                target='_blank'
-                rel='noreferrer'
-                className='project-links'
-                id='nibbler-readme'
-              >
-                <AiIcons.AiOutlineGithub />
-                View Readme
-              </a>
-            </div>
+        <div className='project-info'>
+          <h1 className='project-titles'>Nibbler Bytes</h1>
+          <p>
+            a 7 day solo project to test the skills I had learned up until that
+            point. The remit of the task was to build a grid based game using
+            Vanilla JavaScript, HTML and CSS. Links to the project and readme
+            can be found below:
+          </p>
+          <div className='project-links'>
+            <a
+              href='https://bit.ly/37ZMiKK'
+              target='_blank'
+              className='project-links'
+              rel='noreferrer'
+              id='nibbler-website'
+            >
+              <BsIcons.BsLink45Deg />
+              Go to Website
+            </a>
+            <br />
+            <a
+              href='https://bit.ly/3PHo1df'
+              target='_blank'
+              rel='noreferrer'
+              className='project-links'
+              id='nibbler-readme'
+            >
+              <AiIcons.AiOutlineGithub />
+              View Readme
+            </a>
           </div>
         </div>
       </div>
     </main>
-    // </section>
   );
 }
 export default Projects;
